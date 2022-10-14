fe2a5a25f96b36eada757c56d30b0096ac25e2cb
Matteo Mansi <matteo.mansi@edu.unito.it>
2022-10-14 17:45:07 +0200

    added pom

---
+++ SingletonExample/src/test/java/UserTest.java
@@ -0,0 +1,59 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    User user = User.getInstance();
+    @Before
+    public void setUp() throws Exception {
+        user.setId(1);
+        user.setAge(22);
+        user.setFirstName("Matteo");
+        user.setLastName("Mansi");
+    }
+
+    @Test
+    public void testInstance(){
+        User user = User.getInstance();
+        User user2 = User.getInstance();
+        assertEquals(user,user2);
+    }
+
+    @Test
+    public void getId() {
+        assertNotNull(user.getId());
+    }
+
+    @Test
+    public void setId() {
+        long old_id = user.getId();
+        user.setId(2);
+        assertNotEquals(user.getId(),old_id);
+    }
+
+    @Test
+    public void getFirstName() {
+        assertNotNull(user.getFirstName());
+    }
+
+    @Test
+    public void setFirstName() {
+        String old_name = user.getFirstName();
+        user.setFirstName("Mario");
+        assertNotEquals(user.getFirstName(),old_name);
+    }
+
+    @Test
+    public void getAge() {
+        assertNotNull(user.getAge());
+    }
+
+    @Test
+    public void setAge() {
+        int old_age = user.getAge();
+        user.setAge(old_age+1);
+        assertNotEquals(user.getAge(),old_age);
+    }
+}
