3a10a9c1ebb6a96280a397ecd630b79e6ee3990e
Matteo Mansi <matteo.mansi@edu.unito.it>
2022-10-16 10:59:31 +0200

    first trial with springboot

---
+++ Spring_Example/src/main/java/com/example/Spring_Example/Repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.Spring_Example.Repository;
+
+import com.example.Spring_Example.Model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
